6f23b676cdb4e26521028f41d3bd7db03afcd7df
MaedaKei <runnerkei3000@ezweb.ne.jp>
2025-11-27 14:35:19 +0900

    OFF detach

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {app,BrowserWindow,dialog,ipcMain,screen} = require('electron');
 const path = require('path');
 const fs = require('fs');
-const detach=true;
+const detach=false;
 const WindowManager=new Map();
 //メインウィンドウの作成
 function createMainWindow(){
