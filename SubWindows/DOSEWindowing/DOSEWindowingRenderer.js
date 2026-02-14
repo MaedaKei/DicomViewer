4685912d376adf20ecb3ac14cf5ecfa4bd4bd9f2
MaedaKei <runnerkei3000@ezweb.ne.jp>
2026-02-14 21:09:50 +0900

    [fix]Tabでフォーカスを移動させると値が勝手に変わるバグを修正

--- SubWindows/DOSEWindowing/DOSEWindowingRenderer.js
+++ SubWindows/DOSEWindowing/DOSEWindowingRenderer.js
@@ -326,7 +326,7 @@ class DOSEWindowingClass{
             //const newvmax=parseInt(this.MaxValueInput.value);
             const CurrentInputValue=parseFloat(this.TargetDoseGyInput.value);
             const PreviousInputValue=parseFloat(this.TargetDoseGyInput.getAttribute("data-PreviousValue"));
-            if(CurrentInputValue!==PreviousInputValue||true){
+            if(CurrentInputValue!==PreviousInputValue){
                 const NewTargetDoseGy=CurrentInputValue;
                 const NewLowerLimitDoseGy=this.CurrentLowerLimitDoseGy;
                 this.CheckAndSetValues(NewTargetDoseGy,NewLowerLimitDoseGy);
@@ -348,7 +348,7 @@ class DOSEWindowingClass{
         const LowerLimitDoseGyInputFunction=()=>{
             const CurrentInputValue=parseFloat(this.LowerLimitDoseGyInput.value);
             const PreviousInputValue=parseFloat(this.LowerLimitDoseGyInput.getAttribute("data-PreviousValue"));
-            if(CurrentInputValue!==PreviousInputValue||true){
+            if(CurrentInputValue!==PreviousInputValue){
                 const NewTargetDoseGy=this.CurrentTargetDoseGy;
                 const NewLowerLimitDoseGy=CurrentInputValue;
                 this.CheckAndSetValues(NewTargetDoseGy,NewLowerLimitDoseGy);
@@ -369,9 +369,9 @@ class DOSEWindowingClass{
         const LowerLimitDoseGyParcentageFunction=()=>{
             const CurrentInputValue=parseFloat(this.LowerLimitDoseParcentageInput.value);
             const PreviousInputValue=parseFloat(this.LowerLimitDoseParcentageInput.getAttribute("data-PreviousValue"));
-            if(CurrentInputValue!==PreviousInputValue||true){//入力欄の値が変わっていれば処理を走らせる
+            if(CurrentInputValue!==PreviousInputValue){//入力欄の値が変わっていれば処理を走らせる
                 const NewTargetDoseGy=this.CurrentTargetDoseGy;
-                const NewLowerLimitDoseParcentage=parseFloat(CurrentInputValue)/100;//%⇒小数
+                const NewLowerLimitDoseParcentage=CurrentInputValue/100;//%⇒小数
                 const NewLowerLimitDoseGy=NewTargetDoseGy*NewLowerLimitDoseParcentage;
                 this.CheckAndSetValues(NewTargetDoseGy,NewLowerLimitDoseGy);
             }
@@ -420,7 +420,9 @@ class DOSEWindowingClass{
         const TargetDoseGyforText=Math.trunc(TargetDoseGy*100)/100;
         const LowerLimitDoseGyforText=Math.trunc(LowerLimitDoseGy*100)/100;
         this.TargetDoseGyInput.value=TargetDoseGyforText;
+        this.TargetDoseGyInput.setAttribute("data-PreviousValue",TargetDoseGyforText);
         this.LowerLimitDoseGyInput.value=LowerLimitDoseGyforText;
+        this.LowerLimitDoseGyInput.setAttribute("data-PreviousValue",LowerLimitDoseGyforText);
         const LowerLimitDoseParcentageforText=Math.trunc(LowerLimitDoseParcentage*100*100)/100
         this.LowerLimitDoseParcentageInput.value=LowerLimitDoseParcentageforText;
         this.LowerLimitDoseParcentageInput.setAttribute("data-PreviousValue",LowerLimitDoseParcentageforText);
